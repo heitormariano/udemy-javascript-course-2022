7dd7e101667a2ee0f8f24204d0b34f7a1782bcb4
Heitor Mariano <heitor.eumesmo@gmail.com>
2021-06-26 20:53:29 -0300

    implementando comportamento do botão hold. Controlando estado do jogo

--- section 7 - DOM_events/projeto-03/script.js
+++ section 7 - DOM_events/projeto-03/script.js
@@ -15,8 +15,12 @@ let currentScore = 0;
 let dice = 0;
 let activePlayer = 0;
 
+let scorePlayers = [0,0];
+let playing = true;
+
 score0.textContent = 0;
 score1.textContent = 0;
+
 diceElem.classList.add('hidden');
 
 const rollDice = function () {
@@ -31,22 +35,48 @@ const updateCurrentScore = function () {
   document.getElementById(`current--${activePlayer}`).textContent = currentScore;
 };
 
+const switchPlayer = function () {
+  currentScore = 0;
+  dice = 0; // my code needs to redefine the dice value
+  
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0; // switch player
+  player0Elem.classList.toggle('player--active');
+  player1Elem.classList.toggle('player--active');
+  updateCurrentScore();
+};
+
 const getCurrentScore = function () {
   // The condition to switch to next player is based in dice value. When the value is 1 occurs the switching
   if (dice !== 1) {
     updateCurrentScore();
   } else {
-    currentScore = 0;
-    dice = 0; // my code needs to redefine the dice value
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0; // switch player
-    player0Elem.classList.toggle('player--active');
-    player1Elem.classList.toggle('player--active');
-    updateCurrentScore();
+    switchPlayer();
   }
 };
 
 btnRoll.addEventListener('click', function () {
-  rollDice();
-  getCurrentScore();
+  if (playing){
+    rollDice();
+    getCurrentScore();
+  }
+});
+
+btnHold.addEventListener('click', function (){
+  if (playing){
+    //calculate score
+    scorePlayers[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = scorePlayers[activePlayer];
+  
+    if (scorePlayers[activePlayer] >= 20){
+      playing = false;
+      diceElem.classList.add('hidden');
+
+      const sectionPlayer = document.querySelector(`.player--${activePlayer}`);
+      sectionPlayer.classList.add('player--winner');
+      sectionPlayer.classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
 });
